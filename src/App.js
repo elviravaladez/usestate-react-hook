f338d5a1865d18e8cb990de9d01c77355f0307a2
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-30 20:25:49 -0500

    feat: useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+    const [count, setCount] = useState(4);
+    const [theme, setTheme] = useState('start');
+
+    const decrementCount = () => {
+        setCount(previousCount => previousCount - 1);
+        setTheme('black');
+    };
+
+    const incrementCount = () => {
+        setCount(previousCount => previousCount + 1);
+        setTheme('green');
+
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <button onClick={decrementCount}>-</button>
+          <span>{count}</span>
+          <span>{theme}</span>
+          <button onClick={incrementCount}>+</button>
+      </div>
   );
 }
 
